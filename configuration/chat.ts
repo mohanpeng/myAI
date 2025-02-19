d1c2a1ca18c54a7affab913e0860298cceb9d01a
mohanpeng <mohanpeng114@gmail.com>
2025-02-19 14:23:11 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,7 +1,7 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
 export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, how can I help you?`;
-export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
+export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Give me a second.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
