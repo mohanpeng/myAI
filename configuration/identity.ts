42ca06117bfc0dc30a653a86c240bed7dd4fb9b3
mohanpeng <mohanpeng114@gmail.com>
2025-02-26 14:17:02 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Jeff`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `AI Assistant on IR Development`;
+export const AI_NAME: string = `AI Assistant on Ukraine Peace Talk Development`;
 export const AI_TONE: string = `Professional`;
 export const AI_ROLE: string = `[AI ROLE]`;
